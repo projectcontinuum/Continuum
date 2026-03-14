5ee07694eb3fe3dacc41fffba77afce32bbe96be
projectcontinuum <projectdevcontinuum@gmail.com>
2026-03-14 16:09:22 +0530

    feat/CONTNM-40: New gradle plugin

---
+++ continuum-gradle-plugin/src/main/kotlin/org/projectcontinuum/gradle/ContinuumExtension.kt
@@ -0,0 +1,30 @@
+package org.projectcontinuum.gradle
+
+import org.gradle.api.provider.Property
+
+abstract class ContinuumExtension {
+
+    /** Version of continuum-commons and continuum-worker-springboot-starter */
+    abstract val continuumVersion: Property<String>
+
+    /** Maven group ID for continuum core artifacts (commons, worker-starter) */
+    abstract val continuumGroup: Property<String>
+
+    /** Spring Boot BOM version */
+    abstract val springBootVersion: Property<String>
+
+    /** Spring Cloud BOM version */
+    abstract val springCloudVersion: Property<String>
+
+    /** Temporal BOM version */
+    abstract val temporalVersion: Property<String>
+
+    /** AWS SDK BOM version */
+    abstract val awsSdkVersion: Property<String>
+
+    /** Jackson module version */
+    abstract val jacksonVersion: Property<String>
+
+    /** Whether to configure Maven Central publishing via JReleaser */
+    abstract val publishToMavenCentral: Property<Boolean>
+}
